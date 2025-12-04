86efcfd55b3a170e627ce2aded504444e3796b1f
rupanmentor <rupanguvimentor@gmail.com>
2025-12-04 19:21:05 +0530

    Register,login completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./Database/dbConfig.js";
+import authRoute from "./Routes/authRoute.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ app.get("/", (req, res) => {
   res.send("Welcome to backend");
 });
 
+app.use("/api/auth", authRoute);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server Started");
 });
+
