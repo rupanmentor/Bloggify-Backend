fb60508406285cd582dabfcfbe170e3f9321c484
rupanmentor <rupanguvimentor@gmail.com>
2025-12-04 21:08:28 +0530

    updated

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, register } from "../Controllers/authController.js";
+import { forgotPassword, login, register, resetPassword } from "../Controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password",forgotPassword);
+router.post("/reset-password/:id/:token",resetPassword);
 
 export default router;
