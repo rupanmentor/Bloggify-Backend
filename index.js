6fd00b12a819875cc9c3c81342bd871ee1963ba5
rupanmentor <rupanguvimentor@gmail.com>
2025-12-08 19:36:12 +0530

    cors updated

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:"*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE","PATCH"]
+}));
 
 connectDB();
 
